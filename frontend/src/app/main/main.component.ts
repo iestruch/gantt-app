dba6f6d062d7b82491656482fdc56d0d4a554d1a
Ivan Estruch Peiro <ivanestruch@gmail.com>
2021-02-16 22:42:22 +0100

    invert by project

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -4,6 +4,7 @@ import { of, Subject } from 'rxjs';
 import { catchError, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { DevelopersService } from './developers/developers.service';
 import { IssuesService } from './issues/issues.service';
+import { ProjectsService } from './projects/projects.service';
 
 @Component({
   selector: 'app-main',
@@ -14,11 +15,13 @@ export class MainComponent implements OnInit, OnDestroy {
   unsubscribe$: Subject<void> = new Subject<void>();
   issues: any[];
   developers: any[];
+  projects: any[];
   teams: any[];
   teamSelected: string;
   chartData: any[];
   loading: boolean;
   planning: boolean;
+  groupVariable: 'developers' | 'projects';
   updating: {
     enabled: boolean;
     value: any;
@@ -26,7 +29,8 @@ export class MainComponent implements OnInit, OnDestroy {
   constructor(
     private route: Router,
     private issuesService: IssuesService,
-    private developersService: DevelopersService
+    private developersService: DevelopersService,
+    private projectsService: ProjectsService
   ) { }
 
   ngOnInit(): void {
@@ -36,11 +40,13 @@ export class MainComponent implements OnInit, OnDestroy {
   initData(): void {
     this.issues = [];
     this.developers = [];
+    this.projects = [];
     this.teams = [];
     this.teamSelected = '0';
     this.chartData = [];
     this.loading = true;
     this.planning = false;
+    this.groupVariable = 'developers'
     this.updating = {
       enabled: false,
       value: null
@@ -52,6 +58,11 @@ export class MainComponent implements OnInit, OnDestroy {
           const { developers } = data as any;
           this.developers = [...developers];
           this.teams = [...new Set(developers.map(dev => dev.team))];
+        }),
+        switchMap(() => this.projectsService.getProjects()),
+        tap(({ data, loading }) => {
+          const { projects } = data as any;
+          this.projects = [...projects];
         }),
         switchMap(() => this.issuesService.getIssues()),
         tap(({ data, loading }) => {
@@ -69,16 +80,7 @@ export class MainComponent implements OnInit, OnDestroy {
           return;
         }
 
-        this.chartData = [
-          ...this.developers.map((item) => ({ id: item.id, name: item.name })),
-          ...this.issues.map((issue) => ({
-            id: issue.id,
-            name: `${issue.projectId.name} - ${issue.label}`,
-            parent: issue.devId.id,
-            start: (new Date(issue.startDate)).getTime(),
-            end: new Date(issue.endDate).getTime()
-          }))
-        ]
+        this.createChartData();
       })
   }
 
@@ -90,10 +92,17 @@ export class MainComponent implements OnInit, OnDestroy {
     this.teamSelected = $event.value;
     this.chartData = [];
     setTimeout(() => {
+      this.createChartData();
+    }, 0)
+  }
+
+  createChartData() {
+    if (this.groupVariable === 'developers') {
       this.chartData = [
         ...this.developers
           .filter((dev) => this.teamSelected === '0' ? dev : dev.team.id === this.teamSelected)
           .map((item) => ({ id: item.id, name: item.name })),
+
         ...this.issues
           .filter((issue) => this.teamSelected === '0' ? issue : issue.devId.team.id === this.teamSelected)
           .map((issue) => ({
@@ -102,25 +111,49 @@ export class MainComponent implements OnInit, OnDestroy {
             parent: issue.devId.id,
             start: (new Date(issue.startDate)).getTime(),
             end: new Date(issue.endDate).getTime()
-          })
-          )
+          }))
       ];
-    }, 0)
+    } else if (this.groupVariable === "projects") {
+      this.chartData = [
+        ...this.projects
+          .map((item) => ({ id: item.id, name: item.name })),
+        ...this.issues
+          .filter((issue) => this.teamSelected === '0' ? issue : issue.devId.team.id === this.teamSelected)
+          .map((issue) => ({
+            id: issue.id,
+            name: `${issue.devId.name} - ${issue.label}`,
+            parent: issue.projectId.id,
+            start: (new Date(issue.startDate)).getTime(),
+            end: new Date(issue.endDate).getTime()
+          }))
+      ];
+    }
+  }
+
+  invert() {
+    this.loading = true;
+    this.chartData = [];
+    this.groupVariable = this.groupVariable === 'developers' ? 'projects' : 'developers';
+    setTimeout(() => {
+      this.loading = false;
+      this.createChartData();
+    },300)
+    
   }
 
   addedPlan(value) {
     this.loading = true;
     this.planning = false;
-    this.updating = {...this.updating, enabled: false, value: null};
-    if (!value) {return;}
+    this.updating = { ...this.updating, enabled: false, value: null };
+    if (!value) { return; }
     this.chartData = [];
     this.reloadIssues(this.teamSelected);
   }
 
   issueClicked($event) {
     const issue = this.issues.find(issue => issue.id === $event.id);
-    if (!issue) {return;}
-    this.updating = {...this.updating, enabled: true, value: {...issue}};
+    if (!issue) { return; }
+    this.updating = { ...this.updating, enabled: true, value: { ...issue } };
   }
 
   reloadIssues(teamFilter: string) {
@@ -141,21 +174,7 @@ export class MainComponent implements OnInit, OnDestroy {
 
         setTimeout(() => {
           this.issues = [...issues];
-          this.chartData = [
-            ...this.developers
-              .filter((dev) => this.teamSelected === '0' ? dev : dev.team.id === this.teamSelected)
-              .map((item) => ({ id: item.id, name: item.name })),
-            ...issues
-              .filter((issue) => this.teamSelected === '0' ? issue : issue.devId.team.id === this.teamSelected)
-              .map((issue) => ({
-                id: issue.id,
-                name: `${issue.projectId.name} - ${issue.label}`,
-                parent: issue.devId.id,
-                start: (new Date(issue.startDate)).getTime(),
-                end: new Date(issue.endDate).getTime()
-              }))
-          ]
-
+          this.createChartData();
         }, 300)
       })
   }
